f9f25b0186154f6efe7c43185b0ae9a2c381c635
Won Sub Jung <71582032+aaron-rev@users.noreply.github.com>
2023-12-15 16:02:04 +0900

    Add files via upload

---
+++ bike.sql
@@ -0,0 +1,124 @@
+use bike;
+
+-- BoothInfo 테이블 생성
+CREATE TABLE BoothInfo (
+    BNo INT, -- 부스 번호 (고유 식별자)
+    BName VARCHAR(50), -- 부스 이름
+	RoadLocation VARCHAR(100), -- 도로명주소
+    BLocation VARCHAR(50), -- 부스 위치
+    Latitude DECIMAL(10, 6), -- 위도
+    Longitude DECIMAL(10, 6), -- 경도
+    Install_date DATE, -- 설치 날짜
+	Install_type VARCHAR(10), -- 설치 유형 (LCD 또는 QR)
+    LCDNo INT(3),
+    QRNo INT(3),
+    PRIMARY KEY (BNo, BName, BLocation)
+);
+
+-- BoothMaster 테이블 생성
+CREATE TABLE BoothMaster (
+    BID VARCHAR(30) PRIMARY KEY, -- 부스 마스터의 고유 식별자
+    RoadLocation VARCHAR(100), -- 도로명주소
+    BLocation VARCHAR(50), -- 부스 마스터의 위치
+    Longitude DECIMAL(10, 6), -- 경도
+    Latitude DECIMAL(10, 6) -- 위도 (없는 대여소면 둘다 0)
+);
+
+-- Riding 테이블 생성
+CREATE TABLE Riding (
+    Start_BID INT, -- 시작 부스 식별자
+    Start_BName VARCHAR(50), -- 시작 부스 이름
+    Finish_BID INT, -- 종료 부스 식별자
+    Finish_BName VARCHAR(50), -- 종료 부스 이름
+    TravelDate DATE, -- 여행 날짜
+    BorrowTime TIME, -- 여행 시작 시간
+    UsedTime TIME, -- 여행 시간
+    UsedDistance DECIMAL(10, 2), -- 이동 거리
+    PRIMARY KEY (Start_BID, Finish_BID),
+    FOREIGN KEY (Start_BID, Start_BName) REFERENCES BoothInfo (BNo, BName), -- BoothInfo에 대한 외래 키
+    FOREIGN KEY (Finish_BID, Finish_BName) REFERENCES BoothInfo (BNo, BName) -- BoothInfo에 대한 외래 키
+);
+
+-- 즐겨찾기 // BoothMaster와 내용 동일
+CREATE TABLE favorites (
+    BID VARCHAR(30) PRIMARY KEY, -- 부스 마스터의 고유 식별자
+    RoadLocation VARCHAR(100), -- 도로명주소
+    BLocation VARCHAR(50), -- 부스 마스터의 위치
+    Longitude DECIMAL(10, 6), -- 경도
+    Latitude DECIMAL(10, 6) -- 위도 (없는 대여소면 둘다 0)
+);
+
+/*
+ALTER TABLE favorites DROP COLUMN Longitude;
+ALTER TABLE favorites DROP COLUMN Latitude;
+*/
+
+-- 회원 테이블
+CREATE TABLE userlist (
+    asdf
+    asdf
+    asdf
+    asdf
+    adsf
+);
+
+
+/* Usage 테이블 생성
+CREATE TABLE Usage (
+    Start_BID INT, -- 시작 부스 식별자
+    Start_BName VARCHAR(50), -- 시작 부스 이름
+    Finish_BID INT, -- 종료 부스 식별자
+    Finish_BName VARCHAR(50), -- 종료 부스 이름
+    TravelDate DATE, -- 여행 날짜
+    BorrowTime TIME, -- 여행 시작 시간
+    UsedTime TIME, -- 사용 시간
+    UsedDistance DECIMAL(10, 2), -- 이동 거리
+    FOREIGN KEY (Start_BID, Start_BName) REFERENCES BoothInfo(BNo, BName), -- BoothInfo에 대한 외래 키
+    FOREIGN KEY (Finish_BID, Finish_BName) REFERENCES BoothInfo(BNo, BName) -- BoothInfo에 대한 외래 키
+);
+*/
+
+-- 전체 리스트 보기 / BoothInfo 확인용
+SELECT *
+FROM BOOTHINFO
+
+-- 대여소 정보 조회
+-- (혹시 몰라서) BNo로 찾기
+SELECT BNo -- 실제로 검색하면 도로명을 띄워줌
+FROM BOOTHINFO
+WHERE ID = 000 -- 000 -> (input)
+
+-- 부스 이름으로 찾기
+SELECT BName -- BNo 를 띄워도 되긴 함
+FROM BOOTHINFO
+WHERE BName = station -- station = 입력값
+
+-- 대여소 정보 조회
+SELECT BID, RoadLocation, BLocation
+FROM BoothMaster
+WHERE Longitude NOT 0 AND Latitude NOT 0 
+-- 위도, 경도 0 : 없는 대여소 제외 목적
+
+-- BoothMaster 확인용
+SELECT *
+FROM BOOTHMASTER
+
+-- Riding 확인용
+SELECT *
+FROM Riding
+
+-- 회원가입
+INSERT INTO userlist (UID, pw, username, HP) -- HP = 핸드폰 번호
+/*FROM
+WHERE*/
+
+
+-- 즐겨찾기 추가
+SELECT qwer
+FROM asdf 
+WHERE BName = station -- station = 입력값
+
+-- 즐겨찾기 제거 //원본 사이트에서는 제대로 동작하지 않음
+SELECT
+FROM favorites
+WHERE
